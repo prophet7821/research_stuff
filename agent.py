58f42ea8ca74064e8fe93f4ee529a4c5509c4b11
Samridha Das <samridhadas782001@gmail.com>
2025-04-10 00:05:41 +0530

    red team zero shot

--- agent.py
+++ agent.py
@@ -1,6 +1,4 @@
-import os
 import json
-import torch
 from abc import ABC, abstractmethod
 from transformers import AutoModelForCausalLM, AutoTokenizer, pipeline
 from utils import generate_response_llama
