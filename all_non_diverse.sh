e473ea0cae4c5fe444fac8fa7b0582db5472fc85
Samridha Das <samridhadas782001@gmail.com>
2025-04-12 10:09:02 +0000

    .

---
+++ all_non_diverse.sh
@@ -0,0 +1,4 @@
+python red_teaming_entry.py -s 5 -e --input_path ./data/red_teams.txt -r 5
+python red_teaming_entry.py -s 6 -e --input_path ./data/red_teams.txt -r 5
+python red_teaming_entry.py -s 7 -e --input_path ./data/red_teams.txt -r 5
+python red_teaming_entry.py -s 8 -e --input_path ./data/red_teams.txt -r 5
---
+++ all_zero_shot.sh
@@ -0,0 +1,4 @@
+python red_teaming_entry.py -s 1 -e --input_path ./data/red_teams.txt 
+python red_teaming_entry.py -s 2 -e --input_path ./data/red_teams.txt 
+python red_teaming_entry.py -s 3 -e --input_path ./data/red_teams.txt 
+python red_teaming_entry.py -s 4 -e --input_path ./data/red_teams.txt
